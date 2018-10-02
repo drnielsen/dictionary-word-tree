e6616332708177245351c536e9f9e55edefb6bca
Dane R. Nielsen <dane@danernielsen.com>
2018-10-02 18:51:27 -0500

    Write to graph dot file

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,34 @@ use std::fmt;
 use std::iter::Iterator;
 use std::cmp::Ordering;
 use std::cmp::min;
+use std::error::Error;
+use std::io::prelude::*;
+use std::path::Path;
+
 
 fn main() {
     println!("{}", "Opening file");
-    let f = File::open("/Users/drnielsen/Development/misc/english-words/words.txt").unwrap();
+    //let f = File::open("/Users/drnielsen/Development/misc/english-words/words_alpha.txt").unwrap();
+    let f = File::open("/usr/share/dict/words").unwrap();
     let buffered_reader = BufReader::new(f);
 
-    let dictionary: HashSet<String> = buffered_reader.lines().map(|x| x.unwrap()).collect();
+    let dictionary: HashSet<String> = buffered_reader.lines()
+                                                     .map(|x| x.unwrap())
+                                                     .map(|x| x.to_lowercase())
+                                                     .filter(|x| {
+                                                        if x.len() > 1 {
+                                                            return true
+                                                        } else if x == "a" {
+                                                            return true
+                                                        } else if x == "o" {
+                                                            return true
+                                                        } else if x == "i" {
+                                                            return true
+                                                        } else {
+                                                            return false
+                                                        }
+                                                     })
+                                                     .collect();
 
     let count = dictionary.iter().count();
     println!("Found {} word total.", count);
@@ -22,9 +43,57 @@ fn main() {
 
     let results = build_word_tree(&dictionary, &nine_letter_words, 8);
 
-    println!("Final count: {}", results.iter().count());
     println!("Filtered words:");
     results.iter().for_each(|word_lineage| println!("{}", word_lineage));
+
+    println!("Words List:");
+    let mut words: Vec<String> = results.iter().map(|x| x.get_original_word().to_string()).collect();
+    words.sort_unstable();
+    words.dedup();
+    words.iter().for_each(|word| println!("{}", word));
+    println!("Final count: {}", words.iter().count());
+
+    let mut dotfile_instructions: Vec<String> = results.iter().flat_map(|x| x.get_dot_file_instructions()).collect();
+    dotfile_instructions.sort_unstable();
+    dotfile_instructions.dedup();
+    //dotfile_instructions.iter().for_each(|word| println!("{}", word));
+
+    let path = Path::new("word_graph.dot");
+    let display = path.display();
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                           display,
+                           why.description()),
+        Ok(file) => file,
+    };
+
+    match file.write_all("digraph G {\n".as_bytes()) {
+        Err(why) => {
+                panic!("couldn't write to {}: {}", display,
+                                                why.description())
+            },
+            Ok(_) => (),
+    }
+
+    for instruction in dotfile_instructions {
+        match file.write_all((instruction+"\n").as_bytes()) {
+            Err(why) => {
+                panic!("couldn't write to {}: {}", display,
+                                                why.description())
+            },
+            Ok(_) => (),
+        }
+    }
+
+    match file.write_all("}\n".as_bytes()) {
+        Err(why) => {
+                panic!("couldn't write to {}: {}", display,
+                                                why.description())
+            },
+            Ok(_) => (),
+    }
 }
 
 fn build_word_tree(dictionary: &HashSet<String>, lineage_list: &Vec<&String>, iterations: i32) -> Vec<WordLineage> {
@@ -32,7 +101,7 @@ fn build_word_tree(dictionary: &HashSet<String>, lineage_list: &Vec<&String>, it
 
     for _ in 0..iterations {
         pipeline = Box::new(pipeline.flat_map(|word| create_word_permutations(&word))
-                .filter(|word_lineage| is_a_word(word_lineage.get_word(), &dictionary)));
+                .filter(|word_lineage| is_a_word(word_lineage.get_latest_word(), &dictionary)));
     }
     let mut result: Vec<WordLineage> = pipeline.collect();
     result.sort_unstable();
@@ -42,7 +111,7 @@ fn build_word_tree(dictionary: &HashSet<String>, lineage_list: &Vec<&String>, it
 
 fn create_word_permutations(lineage_to_permutate: &WordLineage) -> Vec<WordLineage> {
     let mut permutations: Vec<String> = Vec::new();
-    let str_to_mutate = lineage_to_permutate.get_word();
+    let str_to_mutate = lineage_to_permutate.get_latest_word();
 
     for x in 0..str_to_mutate.len(){
         let mut substring = str_to_mutate[..x].to_string();
@@ -70,8 +139,24 @@ impl WordLineage {
     }
 
     /// Get the most recent word from this lineage
-    fn get_word(&self) -> &str {
+    fn get_latest_word(&self) -> &str {
         &self.lineage[self.lineage.len()-1]
+    }
+
+    /// Get the original word from this lineage
+    fn get_original_word(&self) -> &str {
+        &self.lineage[0]
+    }
+
+    fn get_dot_file_instructions(&self) -> Vec<String> {
+        let mut instructions = Vec::new();
+        for x in 0..self.lineage.len()-1 {
+            let mut instruct = self.lineage[x].to_string();
+            instruct.push_str(" -> ");
+            instruct.push_str(&self.lineage[x+1]);
+            instructions.push(instruct);
+        }
+        instructions
     }
 }
 
@@ -121,14 +206,10 @@ impl fmt::Display for WordLineage {
         match it.next() {
             None => output.push_str("Lineage is empty"),
             Some(first_word) => {
-                output.push('\'');
                 output.push_str(first_word);
-                output.push('\'');
                 it.for_each(|x| {
                     output.push_str(" -> ");
-                    output.push('\'');
                     output.push_str(x);
-                    output.push('\'');
                 });
             },
         };
